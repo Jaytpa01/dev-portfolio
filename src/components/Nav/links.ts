8aad6f8b2719a1b3474efbe3e6917be03a181584
Jay <jaytpa01@gmail.com>
2023-12-23 22:52:27 +1100

    feat: started building nav components

---
+++ src/components/Nav/links.ts
@@ -0,0 +1,23 @@
+interface link {
+	label: string;
+	href: string;
+}
+
+export const links: link[] = [
+	{
+		label: "Resume",
+		href: "/resume",
+	},
+	{
+		label: "Projects",
+		href: "/projects",
+	},
+	{
+		label: "Blog",
+		href: "/blog",
+	},
+	{
+		label: "Resources",
+		href: "/resources",
+	},
+];
