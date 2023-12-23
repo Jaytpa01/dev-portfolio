ac30f19d1ec8c69fc2968bcec560c46edd405739
Jay <jaytpa01@gmail.com>
2023-12-23 22:51:40 +1100

    chore: installed font "reem kufi"

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["Reem Kufi", ...defaultTheme.fontFamily.sans],
+			},
+			colors: {
+				"neo-red": "#ff7a64",
+				"neo-yellow": "#ffef6b",
+			},
+		},
 	},
 	plugins: [],
 };
