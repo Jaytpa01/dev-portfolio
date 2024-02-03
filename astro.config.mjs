96c9dbf20c328a043d83168784beddadb240c298
Jaytpa01 <jaytpa01@gmail.com>
2024-02-03 22:19:18 +1100

    updated site value

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://neo.jayparry.dev",
+	site: "https://jayparry.dev",
 	integrations: [tailwind(), sitemap()],
 });
