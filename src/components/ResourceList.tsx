a869b4b9ecd06287db86c0bb70953bbf8746f96a
Jay <jaytpa01@gmail.com>
2024-02-09 22:54:09 +1100

    building ResourceList component

--- src/components/ResourceList.tsx
+++ src/components/ResourceList.tsx
@@ -1,3 +1,5 @@
+import { useState } from "preact/hooks";
+
 export interface Folder {
 	id: string;
 	name: string;
@@ -12,6 +14,35 @@ export interface Resource {
 	folderId: string;
 }
 
-export function ResourceList() {
-	return <div>Resource List</div>;
+interface ResourceListProps {
+	rootFolderId: string;
+	folderMap: Record<string, Folder>;
+	subfoldersMap: Record<string, string[]>;
+	folderResourceMap: Record<string, Resource[]>;
+}
+
+export function ResourceList({
+	rootFolderId,
+	folderMap,
+	subfoldersMap,
+	folderResourceMap,
+}: ResourceListProps) {
+	const [currentFolder, setCurrentFolder] = useState(folderMap["articles"]);
+
+	const subfolders = subfoldersMap[currentFolder.id].map(
+		(subfolderId) => folderMap[subfolderId],
+	);
+	const resources = folderResourceMap[currentFolder.id];
+
+	return (
+		<div>
+			<div>Resource List</div>
+			Current:
+			<pre>{JSON.stringify(currentFolder, null, "\t")}</pre>
+			Sub:
+			<pre>{JSON.stringify(subfolders, null, "\t")}</pre>
+			Resources:
+			<pre>{JSON.stringify(resources, null, "\t")}</pre>
+		</div>
+	);
 }
