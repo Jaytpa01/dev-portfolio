7bbc8eab160e60337f88711eee3709de3f903a16
Jay <jaytpa01@gmail.com>
2024-02-10 13:34:11 +1100

    feat: finished implementing core functionality of resource viewer

--- src/components/ResourceList.tsx
+++ src/components/ResourceList.tsx
@@ -3,46 +3,143 @@ import { useState } from "preact/hooks";
 export interface Folder {
 	id: string;
 	name: string;
-
 	parentFolder?: string;
 }
 
 export interface Resource {
 	name: string;
 	href: string;
-
 	folderId: string;
 }
 
-interface ResourceListProps {
+interface ResourcesProps {
 	rootFolderId: string;
 	folderMap: Record<string, Folder>;
-	subfoldersMap: Record<string, string[]>;
+	subfolderMap: Record<string, string[]>;
 	folderResourceMap: Record<string, Resource[]>;
 }
 
-export function ResourceList({
+export function Resources({
 	rootFolderId,
 	folderMap,
-	subfoldersMap,
+	subfolderMap,
 	folderResourceMap,
-}: ResourceListProps) {
-	const [currentFolder, setCurrentFolder] = useState(folderMap["articles"]);
+}: ResourcesProps) {
+	const [currentFolderId, setCurrentFolderId] = useState(rootFolderId);
 
-	const subfolders = subfoldersMap[currentFolder.id].map(
+	const currentFolder = folderMap[currentFolderId];
+
+	const calculateBreadcrumbs = () => {
+		const crumbArray = [];
+		let parentFolderId = currentFolder.parentFolder;
+		while (parentFolderId) {
+			const parentFolder = folderMap[parentFolderId];
+			crumbArray.push(parentFolder);
+
+			parentFolderId = parentFolder.parentFolder;
+		}
+
+		return crumbArray.reverse();
+	};
+	const breadcrumbs = calculateBreadcrumbs();
+	console.log({ breadcrumbs });
+
+	const subfolder = subfolderMap[currentFolder.id]?.map(
 		(subfolderId) => folderMap[subfolderId],
 	);
+
 	const resources = folderResourceMap[currentFolder.id];
 
 	return (
-		<div>
-			<div>Resource List</div>
-			Current:
-			<pre>{JSON.stringify(currentFolder, null, "\t")}</pre>
-			Sub:
-			<pre>{JSON.stringify(subfolders, null, "\t")}</pre>
-			Resources:
-			<pre>{JSON.stringify(resources, null, "\t")}</pre>
-		</div>
+		<>
+			<Breadcrumbs
+				breadcrumbs={breadcrumbs}
+				currentFolder={currentFolder}
+				onCrumbClick={setCurrentFolderId}
+			/>
+
+			<SubfolderList
+				onSubfolderClick={setCurrentFolderId}
+				subfolder={subfolder}
+			/>
+			<ResourceList resources={resources} />
+		</>
+	);
+}
+
+interface BreadcrumbProps {
+	breadcrumbs: Folder[];
+	currentFolder: Folder;
+	onCrumbClick: (folderId: string) => void;
+}
+
+function Breadcrumbs({
+	breadcrumbs,
+	currentFolder,
+	onCrumbClick,
+}: BreadcrumbProps) {
+	return (
+		<ol class="flex space-x-2 font-bold">
+			{breadcrumbs?.map((crumb) => (
+				<li class="space-x-2">
+					<button onClick={() => onCrumbClick(crumb.id)}>{crumb.name}</button>
+
+					<span>{">"}</span>
+				</li>
+			))}
+
+			<li>{currentFolder.name}</li>
+		</ol>
+	);
+}
+
+interface SubfolderListProps {
+	subfolder: Folder[];
+	onSubfolderClick: (subfolderId: string) => void;
+}
+
+function SubfolderList({ subfolder, onSubfolderClick }: SubfolderListProps) {
+	if (!subfolder) {
+		return null;
+	}
+
+	return (
+		<ul class="space-y-2">
+			{subfolder.map((folder) => (
+				<li>
+					<button
+						class="before:pr-1 before:content-['📦']"
+						onClick={() => onSubfolderClick(folder.id)}
+					>
+						{folder.name}
+					</button>
+				</li>
+			))}
+		</ul>
+	);
+}
+
+interface ResourceListProps {
+	resources: Resource[];
+}
+
+function ResourceList({ resources }: ResourceListProps) {
+	if (!resources) {
+		return null;
+	}
+
+	return (
+		<ul class="space-y-2">
+			{resources.map((resource) => (
+				<li>
+					<a
+						class="underline"
+						href={resource.href}
+					>
+						{resource.name}
+					</a>
+				</li>
+			))}
+		</ul>
 	);
 }
