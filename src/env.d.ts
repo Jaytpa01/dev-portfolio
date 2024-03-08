a1f0df5a18a0ee8d6755e9781e651ae226b81459
Jay <jaytpa01@gmail.com>
2024-03-08 22:36:16 +1100

    extend window object in env.d.ts as per docs
    https://docs.astro.build/en/guides/typescript/#extending-window-and-globalthis

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+// extend the window object to keep typescript happy
+interface Window {
+	netlifyIdentity: any;
+}
