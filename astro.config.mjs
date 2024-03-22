ae8d824ab4e2b7b171c1188b6f1d09d37f21e214
Jay <jaytpa01@gmail.com>
2024-03-22 21:12:57 +1100

    Small refactor + add analytics
    removed open sans font-faces
    rename TextLink to HoverLink
    
    install partytown and cloudflare analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import preact from "@astrojs/preact";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://jayparry.dev",
-	integrations: [tailwind(), sitemap(), preact()],
+  site: "https://jayparry.dev",
+  integrations: [tailwind(), sitemap(), preact(), partytown()]
 });
