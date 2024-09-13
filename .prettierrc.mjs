8c05b5915436e6ca081d6170dfbafa1c855fee09
Jay <jaytpa01@gmail.com>
2024-09-13 22:11:08 +1000

    style(prettier): update prettier preferences - use defaults

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,16 +1,14 @@
 /** @type {import("prettier").Config} */
 export default {
-	useTabs: true,
-	tabWidth: 2,
-	singleAttributePerLine: true,
-	plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
-	tailwindFunctions: ["cva"],
-	overrides: [
-		{
-			files: "*.astro",
-			options: {
-				parser: "astro",
-			},
-		},
-	],
+  singleAttributePerLine: true,
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  tailwindFunctions: ["cva"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
 };
